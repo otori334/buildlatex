064b417f3536cdcb9998f2a0e1ae9f1a8b5e008c
otori334 <safujinek@gmail.com>
2020-03-26 04:50:53 +0900

    Nap

--- sh/watcher.sh
+++ sh/watcher.sh
@@ -6,21 +6,41 @@ readonly INTERVAL=1
 readonly PROJECT_DIR=$(cd $(dirname $0); cd ../; pwd) 
 export TARGET_DIRNAME=${1:-md} 
 export no=1 
-cd ${PROJECT_DIR}/src/${TARGET_DIRNAME} || exit 1 
-
-buffer="$(openssl sha256 -r *)" 
+cd ${PROJECT_DIR} 
 
 function aaa() { 
-  local _hash="$(openssl sha256 -r *)" 
-  if [ "${buffer}" != "${_hash}" ]; then 
-    ${PROJECT_DIR}/sh/build.sh & 
+  cd ${PROJECT_DIR}/src/${TARGET_DIRNAME} 
+  local _hash="$(openssl sha256 -r $(ls -p | grep -v /))" 
+  if [ "${_hash}" != "${buffer:=${_hash}}" ]; then 
+    # ${PROJECT_DIR}/sh/build.sh & 
     buffer=${_hash} 
     (( no ++ )) 
+  fi 
+} 
+
+function test() { 
+  cd $1 
+  local _hash="$(openssl sha256 -r $(ls -p | grep -v /))" 
+  FLAG=0 
+  for _sub_dirname in $(ls -p | grep /); do 
+    test ${_sub_dirname} 
+  done 
+
+
+  if [ ${FLAG} -ne 0 -o "${_hash}" != "${buffer:=${_hash}}" ]; then 
+    # ${PROJECT_DIR}/sh/build.sh & 
+    echo "aaaaaaaaaaa${FLAG}_${no}"
+    buffer=${_hash} 
+    (( FLAG ++ )) 
+    (( no ++ )) 
+    
   fi 
 } 
 
 # 監視 
 while true; do 
   sleep ${INTERVAL} 
-  aaa 
+  # aaa 
+  test ${PROJECT_DIR}/src/${TARGET_DIRNAME} 
+  # test src
 done 
