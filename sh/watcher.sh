7761420583a5155263e44a04de912ad2da96640c
otori334 <safujinek@gmail.com>
2020-03-26 12:24:06 +0900

    Refactor - Reduce hash-pattern-length

--- sh/watcher.sh
+++ sh/watcher.sh
@@ -2,18 +2,18 @@
 
 # 監視間隔を秒で指定 
 readonly INTERVAL=1 
+IFS=$'\n' 
 readonly PROJECT_DIR="$(cd $(dirname $0); cd ../; pwd)" 
 export TARGET_DIRNAME="${1:-md}" 
-cd "${PROJECT_DIR}/src/${TARGET_DIRNAME}" 
 export no=1 
-IFS=$'\n' 
+cd "${PROJECT_DIR}/src/${TARGET_DIRNAME}" 
 
 while true; do 
   sleep ${INTERVAL} 
-  hash="$(openssl sha256 -r $(ls -p | grep -v /))" 
-  if [ "${hash}" != "${buffer:=${hash}}" ]; then 
+  hash=$(shasum -a 256 $(ls -p | grep -v /) | shasum -a 256) 
+  if [ ${hash} != ${buffer:=${hash}} ]; then 
     "${PROJECT_DIR}"/sh/build.sh & 
-    buffer="${hash}" 
+    buffer=${hash} 
     (( no ++ )) 
   fi 
 done 
