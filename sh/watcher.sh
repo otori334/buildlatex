bec8fee111e951296bc795de89f8a6541ceca89d
otori334 <safujinek@gmail.com>
2020-03-29 19:51:03 +0900

    Refactor - Watch all files

--- sh/watcher.sh
+++ sh/watcher.sh
@@ -1,46 +1,31 @@
 #!/bin/bash 
 
-set -u 
 # 監視間隔を秒で指定 
-readonly INTERVAL=1 
-readonly PROJECT_DIR=$(cd $(dirname $0); cd ../; pwd) 
-export TARGET_DIRNAME=${1:-md} 
+readonly INTERVAL=0 
+IFS=$'\n' 
+readonly PROJECT_DIR="$(cd $(dirname "$0"); cd ../; pwd)" 
 export no=1 
-cd ${PROJECT_DIR} 
 
-function aaa() { 
-  cd ${PROJECT_DIR}/src/${TARGET_DIRNAME} 
-  local _hash="$(openssl sha256 -r $(ls -p | grep -v /))" 
-  if [ "${_hash}" != "${buffer:=${_hash}}" ]; then 
-    # ${PROJECT_DIR}/sh/build.sh & 
-    buffer=${_hash} 
-    (( no ++ )) 
+function monitor() { 
+  cd "$1" 
+  ((dir_index++)) 
+  local _hash=$(shasum -a 256 $(ls -p | grep -v / || echo "$1") | shasum -a 256) 
+  if [ ${_hash} != ${buffer[${dir_index}]:=${_hash}} ]; then 
+    buffer[${dir_index}]=${_hash} 
+    ((build_flag++)) 
   fi 
-} 
-
-function test() { 
-  cd $1 
-  local _hash="$(openssl sha256 -r $(ls -p | grep -v /))" 
-  FLAG=0 
-  for _sub_dirname in $(ls -p | grep /); do 
-    test ${_sub_dirname} 
+  for _sub_dirname in $(ls -F | grep /); do 
+    monitor "${_sub_dirname}" 
   done 
-
-
-  if [ ${FLAG} -ne 0 -o "${_hash}" != "${buffer:=${_hash}}" ]; then 
-    # ${PROJECT_DIR}/sh/build.sh & 
-    echo "aaaaaaaaaaa${FLAG}_${no}"
-    buffer=${_hash} 
-    (( FLAG ++ )) 
-    (( no ++ )) 
-    
-  fi 
+  cd ../ 
 } 
 
-# 監視 
 while true; do 
   sleep ${INTERVAL} 
-  # aaa 
-  test ${PROJECT_DIR}/src/${TARGET_DIRNAME} 
-  # test src
+  dir_index=0 build_flag=0 
+  monitor "${PROJECT_DIR}/src" 
+  if [ ${build_flag} -ne 0 ]; then 
+    "${PROJECT_DIR}/sh/build.sh" & 
+    ((no++)) 
+  fi 
 done 
